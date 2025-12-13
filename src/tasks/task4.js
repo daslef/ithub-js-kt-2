d19b8fd7b6bd0e84925a3f74012d28b8f12a7d00
spaeeell <spaelbro@gmail.com>
2025-12-14 02:31:43 +0300

    Update task4.js

--- src/tasks/task4.js
+++ src/tasks/task4.js
@@ -4,31 +4,6 @@
  * @description Проверка слова на палиндром
  */
 export function isPalindrome(word) {
-    if (word === null) {
-        throw new Error("Пользователь отменил ввод");
-    }
-    if (typeof word !== 'string') {
-        throw new Error("Некорректные входные данные");
-    }
     
-    const trimmed = word.trim();
-    if (trimmed.length === 0) {
-        throw new Error("Некорректные входные данные");
-    }
-    if (trimmed.includes(' ')) {
-        throw new Error("Введено несколько слов");
-    }
-    if (!/^[а-яА-Яa-zA-Z]+$/.test(word)) {
-        throw new Error("Некорректные входные данные");
-    }
-    
-    if (trimmed.length === 1) return `${trimmed} - не палиндром`;
-
-  const normalizedWord = word.toLowerCase(); //к нижнему
-
-  const reversedWord = normalizedWord.split("").reverse().join(""); //разбивает, переворачивает, объединяет
-
-  return normalizedWord === reversedWord
-    ? `${word} - палиндром`
-    : `${word} - не палиндром`;
+  return result
 }
